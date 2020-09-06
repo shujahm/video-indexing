6feb27158ab57d54a88a913b39f70d5f6276d938
Shuja Ahmed <shujahm@gmail.com>
2020-09-06 15:09:26 +0400

    Initial Commit

---
+++ movieenglish/amazon-sqs-job/src/AmazonSqsJobServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MovieEnglish\AmazonSqsJob;
+
+use Illuminate\Support\ServiceProvider;
+
+class AmazonSqsJobServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/queue.php','queue'
+       );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ movieenglish/amazon-sqs-job/src/app/Jobs/SampleJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MovieEnglish\AmazonSqsJob\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use ShiftOneLabs\LaravelSqsFifoQueue\Bus\SqsFifoQueueable;
+
+class SampleJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, SqsFifoQueueable;
+
+    // The maximum attempts of this job
+    public $tries = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // all the initializtion can happen here
+        Log::info("intiating SampleJob Job");
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // code for processing
+        Log::info("processing SampleJob Job");
+    }
+}
